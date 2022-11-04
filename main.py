36f376ddb2d13728f7557223a031812708738000
Nischit Kumar <class12cscproject@gmail.com>
2022-11-04 18:29:43 +0530

    Main Structure
    Added Menu Options

--- main.py
+++ main.py
@@ -6,7 +6,27 @@ print("                 Welcome To Crypto Arena              ")
 print("               Your Turn To Become Richer             ")
 print("         -------------------------------------        ")
 print("******************************************************")
-print("\n\n\n1.Login")
-print("2.Sign Up")
+print("\n\n\n")
+print("                     1.Existing User? Login           ")
+print("                     2.New User? Sign Up              ")
+print("                     3.Exit                           ")
+
 
 # Main Code Starts From Here
+
+
+# Selection Of Option from Menu
+selection = int(input("Please Enter A Menu Option:"))
+
+# Main Loop
+while True:
+    if selection == 1:
+        pass
+    elif selection == 2:
+        pass
+    elif selection == 3:
+        break
+    else:
+        selection = int(input("Please Enter A Valid Menu Option:"))
+
+print("You've Exited The Program Successfully!")

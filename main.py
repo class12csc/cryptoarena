30fadf950313d9827a86c8f73394f2691156603f
Nischit Kumar <class12cscproject@gmail.com>
2022-11-04 18:16:48 +0530

    Menu Updated
    Created new py file and created a menu file

---
+++ main.py
@@ -0,0 +1,12 @@
+# Printing of menu
+
+print("******************************************************")
+print("         -------------------------------------        ")
+print("                 Welcome To Crypto Arena              ")
+print("               Your Turn To Become Richer             ")
+print("         -------------------------------------        ")
+print("******************************************************")
+print("\n\n\n1.Login")
+print("2.Sign Up")
+
+# Main Code Starts From Here

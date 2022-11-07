9cc83553fb954afe3ed1fdb3cf7d0db38ba5f6b8
Nischit Kumar <class12cscproject@gmail.com>
2022-11-07 06:46:44 +0530

    All code done
    Only fixing of errors are pending!

--- main1.py
+++ main1.py
@@ -57,12 +57,12 @@ def display_submenu():
     print("                     1.Show Balance                   ")
     print("                     2.Deposit Money                  ")
     print("                     3.Withdraw Money                 ")
-    print("                     4.Change username                ")
+    print("                     4.Delete Account                 ")
     print("                     5.Show Available Stocks          ")
     print("                     6.Buy New Stock                  ")
     print("                     7.Add New Stock                  ")
     print("                     8.Remove Existing Stock          ")
-    print("                     9.Return to Main Menu            ")
+    print("                     9.Logout                         ")
 
 
 # Main Code Starts From Here
@@ -72,7 +72,7 @@ display_main()
 selection = int(input("Please enter a menu option:"))
 
 
-def submenu(user):
+def submenu(user, pinno):
     while True:
         """
         1) Show balance
@@ -103,12 +103,17 @@ def submenu(user):
                 print("You've entered an invalid amount.")
                 deposit = input("Please enter a valid amount: ")
 
-            mycur.execute(
-                "SELECT BALANCE FROM USERS WHERE USERNAME = '{}';".format(user))
-            balance_report = mycur.fetchall()
-            net_balance = int(balance_report[0]) + int(deposit)
-            mycur.execute("UPDATE USERS SET BALANCE = '{}' WHERE USERNAME = '{}'".format(
-                str(net_balance), user))
+            pin_entered = input("Please enter your PIN number:")
+            while pin_entered == pinno:
+                print("The entered pin is incorrect.")
+                pin_entered = input("Please enter your correct pin:")
+            else:
+                mycur.execute(
+                    "SELECT BALANCE FROM USERS WHERE USERNAME = '{}';".format(user))
+                balance_report = mycur.fetchall()
+                net_balance = int(balance_report[0]) + int(deposit)
+                mycur.execute("UPDATE USERS SET BALANCE = '{}' WHERE USERNAME = '{}'".format(
+                    str(net_balance), user))
 
         elif selec == 3:
             withdraw = input("Please enter amount of money to be withdrawn:")
@@ -117,15 +122,30 @@ def submenu(user):
                 print("You've entered an invalid amount.")
                 deposit = input("Please enter a valid amount: ")
 
-            mycur.execute(
-                "SELECT BALANCE FROM USERS WHERE USERNAME = '{}';".format(user))
-            balance_report = mycur.fetchall()
-            net_balance = int(balance_report[0]) - int(withdraw)
-            mycur.execute("UPDATE USERS SET BALANCE = '{}' WHERE USERNAME = '{}'".format(
-                str(net_balance), user))
+            pin_entered = input("Please enter your PIN number:")
+            while pin_entered == pinno:
+                print("The entered pin is incorrect.")
+                pin_entered = input("Please enter your correct pin:")
+            else:
+                mycur.execute(
+                    "SELECT BALANCE FROM USERS WHERE USERNAME = '{}';".format(user))
+                balance_report = mycur.fetchall()
+                net_balance = int(balance_report[0]) - int(withdraw)
+                mycur.execute("UPDATE USERS SET BALANCE = '{}' WHERE USERNAME = '{}'".format(
+                    str(net_balance), user))
 
         elif selec == 4:
-            pass
+            replyc = input("Are you sure? Y/N?:")
+            while replyc.isalpha() == False:
+                print("You've entered an invalid option.")
+                replyc = input("Please enter a valid option:")
+            else:
+                if replyc.lower() == "y":
+                    print()
+                    mycur.execute(
+                        "DELETE FROM USERS WHERE USERNAME = '{}'".format(user))
+                else:
+                    print()
 
         elif selec == 5:
             # Displaying the stocks
@@ -144,7 +164,35 @@ def submenu(user):
                     print()
 
         elif selec == 6:
-            pass
+            stock_to_be_bought = input(
+                "Please enter the stock's name you wish to purchase:")
+
+            mycur.execute("SELECT STKNAME FROM STOCKS;")
+            stock_names = mycur.fetchall()
+            while stock_to_be_bought not in stock_names:
+                print("This stock does not exist!")
+                print("Please choose from the above mentioned stocks or add new stock.")
+                add_new_stock = input("Add new Stock? Y/N?")
+                while add_new_stock.isalpha() == False:
+                    print("You've entered an invalid option.")
+                    add_new_stock = input("Please enter a valid option:")
+                if add_new_stock.lower() == "y":
+                    print("You've returned to the main menu")
+                else:
+                    stock_to_be_bought = input(
+                        "Please enter the stock's name from the above mentioned names:")
+            else:
+                pin_entered = input("Please enter your PIN number:")
+                while pin_entered == pinno:
+                    print("The entered pin is incorrect.")
+                    pin_entered = input("Please enter your correct pin:")
+                else:
+                    mycur.execute(
+                        "SELECT BALANCE FROM USERS WHERE USERNAME = '{}';".format(user))
+                    balance_report = mycur.fetchall()
+                    net_balance = int(balance_report[0]) - int(withdraw)
+                    mycur.execute("UPDATE USERS SET BALANCE = '{}' WHERE USERNAME = '{}'".format(
+                        str(net_balance), user))
 
         elif selec == 7:
             # Stock name
@@ -167,11 +215,11 @@ def submenu(user):
                         "Please enter a valid price of the stock (in $):")
 
             # Stock Symbol
-            stksym = input("Please enter a 3-letter symbol for the stock:")
-            while stksym.isalpha() == False or len(stksym) != 3:
+            stksym = input("Please enter a symbol for the stock:")
+            while stksym.isalpha() == False:
                 print("You've entered an invalid symbol.")
                 stksym = input(
-                    "Please enter a valid 3-letter symbol for the stock:")
+                    "Please enter a valid symbol for the stock:")
 
             print(stkval)
 
@@ -182,7 +230,7 @@ def submenu(user):
             print(
                 f"The {stkname} stock has been successfully added to the market!")
 
-        elif selection == 8:
+        elif selec == 8:
 
             # Validating the Stock name
             stkrem = input(
@@ -204,28 +252,172 @@ def submenu(user):
                 print(
                     f"The {stkrem} has been successfully removed from the listing!")
 
-        elif selection == 9:
+        elif selec == 9:
             print("You've successfully returned to the main menu!")
+            break
 
 
 # Main Loop
 while True:
     if selection == 1:
+        # For Username
         mycur.execute("SELECT USERNAME FROM USERS;")
         usernames = mycur.fetchall()
-        print(usernames)
         usrname_existing = input("Please enter your username:")
         print()
-        while (usrname_existing + ",") not in usernames:
+        while usrname_existing not in usernames[0]:
             print("This user doesn't exist!")
             reply = input("New User? Y/N?")
-            if reply.lower() == "y":
-                print("Then please do proceed to the sign up option.")
-                print()
-                break
+            while reply.isalpha() == False:
+                print("You've entered an invalid option.")
+                reply = input("Please enter a valid option:")
             else:
-                usrname_existing = input("Please enter your username again:")
-                print()
+                if reply.lower() == "y":
+                    print("Then please do proceed to the sign up option.")
+                    print()
+                    break
+                else:
+                    usrname_existing = input(
+                        "Please enter your username again:")
+                    print()
+
+        # For Password
+        mycur.execute("SELECT PASSWD FROM USERS;")
+        passwords = mycur.fetchall()
+        pwd_existing = input("Please enter your password:")
+        print()
+        while pwd_existing not in passwords[0]:
+            print("You've entered an incorrect password!")
+            replyp = input("Forgot Password? Y/N?")
+            while replyp.isalpha() == False:
+                print("You've entered an invalid option.")
+                replyp = input("Please enter a valid option:")
+            else:
+                if replyp.lower() == "y":
+                    print("Then please enter the following details.")
+                    print()
+
+                    # Process
+                    accno = input("Please enter your 10-digit account number:")
+
+                    # Validating Account number
+                    while accno.isnumeric() == False or len(accno) != 10:
+                        print("You've entered an incorrent account number.")
+                        accno = input("Please enter a valid account number:")
+
+                    mycur.execute(
+                        "SELECT ACCNO FROM USERS WHERE USERNAME = '{}'".format(usrname_existing))
+                    accno_rec = mycur.fetchall()
+
+                    while accno != accno_rec[0]:
+                        print(
+                            "Your account doesn't exist please create a new account!")
+                        replya = input("New User? Y/N?")
+                        while replya.isalpha() == False:
+                            print("You've entered an invalid option.")
+                            replya = input("Please enter a valid option:")
+                        else:
+                            if replya.lower() == "y":
+                                print(
+                                    "Then please do proceed to the sign up option.")
+                                print()
+                                break
+                            else:
+                                accno = input(
+                                    "Please enter your account number again:")
+                                print()
+                                while accno.isnumeric() == False or len(accno) != 10:
+                                    print(
+                                        "You've entered an incorrent account number.")
+                                    accno = input(
+                                        "Please enter a valid account number:")
+
+                    # Aadhar number
+                    aadhar_no = input("Please enter your Aadhar number:")
+
+                    while aadhar_no.isnumeric() == False:
+                        print("You've entered an invalid Aadhar number.")
+                        aadhar_no = input(
+                            "Please enter a valid Aadhar number: ")
+
+                    mycur.execute(
+                        "SELECT AADHAR FROM USERS WHERE USERNAME = '{}';".format(usrname_existing))
+                    aadhars_of_users = mycur.fetchall()
+
+                    # Checking if Aadhar is repeated
+                    while aadhar_no != aadhars_of_users[0]:
+                        print(
+                            "Your account doesn't exist please create a new account!")
+                        replya = input("New User? Y/N?")
+                        while replya.isalpha() == False:
+                            print("You've entered an invalid option.")
+                            replya = input("Please enter a valid option:")
+                        else:
+                            if replya.lower() == "y":
+                                print(
+                                    "Then please do proceed to the sign up option.")
+                                print()
+                                break
+                            else:
+                                accno = input(
+                                    "Please enter your Aadhar number again:")
+                                print()
+                                while accno.isnumeric() == False or len(accno) != 10:
+                                    print(
+                                        "You've entered an incorrent Aadhar number.")
+                                    accno = input(
+                                        "Please enter a valid Aadhar number:")
+
+                    # PIN number
+                    print("Connecting...")
+                    pin_no = int(input("Please enter your PIN No:"))
+
+                    # Validating PIN number
+                    while len(str(pin_no)) != 4:
+                        print("Please Enter A Valid PIN")
+                        pin_no = int(input("Please Enter Your PIN No:"))
+                        break
+
+                    mycur.execute(
+                        "SELECT PINNO FROM USERS WHERE USERNAME = '{}';".format(usrname_existing))
+                    pin_of_users = mycur.fetchall()
+
+                    # Checking if Aadhar is repeated
+                    while pin_no != pin_of_users[0]:
+                        print(
+                            "Your PIN No doesn't match!")
+                        accno = input(
+                            "Please enter your PIN No again:")
+                        print()
+                        while accno.isnumeric() == False or len(accno) != 4:
+                            print(
+                                "You've entered an incorrent PIN No.")
+                            accno = input(
+                                "Please enter a valid PIN No:")
+
+                    pwd_new = input("Please enter new password:")
+                    pwd_new1 = input("Please enter your password again:")
+
+                    # Checking if the Passwords match
+                    while pwd_new != pwd_new1:
+                        print("Both The Passwords Do Not Match")
+                        pwd_new = input("Please enter new password:")
+                        pwd_new1 = input("Please enter your password again:")
+                    else:
+                        mycur.execute("UPDATE USERS SET PASSWD = '{}' WHERE USERNAME = '{}'".format(
+                            pwd_new, usrname_existing))
+                        print("Your Password has been set successfully!")
+
+                else:
+                    pwd_existing = input(
+                        "Please enter your password again:")
+                    print()
+
+        else:
+            mycur.execute(
+                "SELECT PINNO FROM USERS WHERE USERNAME = '{}'".format(usrname_existing))
+            pin_recs = mycur.fetchall()
+            submenu(usrname_existing, pin_recs[0])
 
     elif selection == 2:
         print("Please enter the following details:-")
@@ -240,13 +432,11 @@ while True:
         username = input("Please enter a username: ")
         mycur.execute("SELECT USERNAME FROM USERS;")
         users = mycur.fetchall()
-        print(users)
-
         # Checking if Username already exists
         while username.lower() in users:
             print("This username has already been taken.")
             username = input("Please Enter Another Username:")
-
+        print()
         # Password
         pwd = input("Please Enter A Password:")
         pwd1 = input("Please Enter The Password Again:")
@@ -262,6 +452,10 @@ while True:
         # Bank Details + Connection
         bankname = input("Please Enter Your Bank Name:")
 
+        while bankname.isalpha() == False:
+            print("You've entered an invalid name.")
+            bankname = input("Please enter your real name: ")
+
         # Checking for 'Bank' at the end
         banktest = bankname.split()
         if banktest[-1].lower() == "bank":
@@ -271,6 +465,11 @@ while True:
 
         # Process
         accno = input("Please enter your 10-digit account number:")
+
+        # Validating Account number
+        while accno.isnumeric() == False or len(accno) != 10:
+            print("You've entered an incorrent account number.")
+            accno = input("Please enter a valid account number:")
 
         # Aadhar number
         aadhar = input("Please enter your Aadhar number:")
@@ -285,27 +484,42 @@ while True:
         # Checking if Aadhar is repeated
         while username.lower() in users:
             print("This Aadhar has already exists.")
-            reply = input("Existing User? Y/N?")
-            if reply.lower() == "y":
-                print("Then please do proceed to the existing user option.")
-                print()
-                break
+            aad_reply = input("Existing User? Y/N?")
+            while aad_reply.isalpha() == False:
+                print("You've entered an invalid option.")
+                aad_reply = input("Please enter a valid option:")
             else:
-                aadhar = input("Please enter your aadhar number again:")
-                print()
+                if reply.lower() == "y":
+                    print("Then please do proceed to the existing user option.")
+                    print()
+                    break
+                else:
+                    aadhar = input("Please enter your aadhar number again:")
+                    print()
 
-        # Validating Account number
-        while accno.isnumeric() == False or len(accno) != 10:
-            print("You've entered an incorrent account number.")
-            accno = input("Please enter a valid account number:")
-
+        # PIN number
         print("Connecting...")
-        pin = int(input("Please Enter Your Pin No:"))
+        pin1 = int(input("Please enter a PIN No:"))
+        pin2 = int(input("Please retype your PIN No to confirm:"))
 
-        # Validating PIN number
-        while len(str(pin)) != 4:
-            print("Please Enter A Valid PIN")
-            pin = int(input("Please Enter Your PIN No:"))
+        # Checking if the PIN No do match
+        while pin1 != pin1:
+            print("Both The PIN Numbers Do Not Match")
+            pin1 = int(input("Please enter a PIN No:"))
+
+            # Validating PIN number
+            while len(str(pin1)) != 4:
+                print("Please Enter A Valid PIN")
+                pin1 = int(input("Please Enter Your PIN No:"))
+
+            pin2 = int(input("Please retype your PIN No to confirm:"))
+
+            # Validating PIN number
+            while len(str(pin2)) != 4:
+                print("Please Enter A Valid PIN")
+                pin2 = int(input("Please Enter Your PIN No:"))
+        else:
+            print("Your Password has been set successfully!")
 
         # Balance
         balance = input(
@@ -316,12 +530,12 @@ while True:
             balance = input("Please Enter a valid amount:")
 
         mycur.execute(
-            "INSERT INTO USERS VALUES ('{}','{}','{}','{}', '{}', '{}', '{}', '{}')".format(accno, bankname[0], pin, name, username, pwd, aadhar, balance))
+            "INSERT INTO USERS VALUES ('{}','{}','{}','{}', '{}', '{}', '{}', '{}')".format(accno, bankname[0], str(pin1), name, username, pwd, aadhar, balance))
         print()
         print(
             f"You've successfully connected to your account {accno}, of {bankname[0]} bank and deposited ${balance} !")
 
-        submenu(username)
+        submenu(username, pin1)
 
     elif selection == 3:
         break
